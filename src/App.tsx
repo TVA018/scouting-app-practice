eabaedd9ee327f260ad1a4a3dcf92b94c5717162
TVA018 <thomas.vu018@gmail.com>
2025-03-16 23:37:11 -0400

    Upload files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './global.css'
+
+function App() {
+  return (
+    <div className="flex flex-col w-full justify-center">
+        <h1 className="text-lg text-center">
+            Home
+        </h1>
+    </div>
+  )
+}
+
+export default App
