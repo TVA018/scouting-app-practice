458ef1789ddbb8cc3e2d8583699fdd06720513c1
TVA018 <thomas.vu018@gmail.com>
2025-03-19 19:29:06 -0400

    Fix dynamic scaling LabeledTextInput

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,16 @@ function App() {
             Home
         </h1>
       </div>
-      <div className='flex flex-row w-full justify-center gap-10 my-4'>
-        <LabeledTextInput id="scouter-name" name="scouter-name" width='10'>Scouter Name</LabeledTextInput>
-      </div>
-      <div className='flex flex-row w-full justify-center gap-10 my-4'>
-        <LabeledTextInput id="team-number" name="team-number" width='10'>Team<br/>Number</LabeledTextInput>
-        <LabeledTextInput id="match-number" name="match-number" width='10'>Match<br/>Number</LabeledTextInput>
-      </div>
+
+      <form>
+        <div className='flex flex-row w-full justify-center gap-10 my-4'>
+          <LabeledTextInput id="scouter-name" name="scouter-name" width='w-xs'>Scouter Name</LabeledTextInput>
+        </div>
+        <div className='flex flex-row w-full justify-center gap-10 my-4'>
+          <LabeledTextInput id="team-number" name="team-number" width='w-40'>Team<br/>Number</LabeledTextInput>
+          <LabeledTextInput id="match-number" name="match-number" width='w-40'>Match<br/>Number</LabeledTextInput>
+        </div>
+      </form>
     </>
   )
 }
--- src/components/LabeledTextInput.tsx
+++ src/components/LabeledTextInput.tsx
@@ -1,3 +1,5 @@
+import '../global.css'
+
 interface Properties {
     children: any;
     id: string;
@@ -5,11 +7,13 @@ interface Properties {
     width?: string;
 }
 
-const LabeledTextInput = ({id, name, children, width="100"}: Properties) => {
+const LabeledTextInput = ({id, name, children, width}: Properties) => {
+  const divTailwindCSS = `flex flex-col ${width} align-center`
+
   return (
-    <div className={`flex flex-col w-${width} align-center`}>
-        <label className="text-md text-center" htmlFor={id}>{children}</label>
-        <input type="text" className='border-b-2 border-white text-md text-center' id={id} name={name}></input>
+    <div className={divTailwindCSS}>
+        <label className="text-md text-center w-full" htmlFor={id}>{children}</label>
+        <input type="text" className='border-b-2 border-white text-md text-center w-full' id={id} name={name}></input>
     </div>
   )
 }
