58f5ec28f7d2fee262517524ea063cd1dc263c73
TVA018 <thomas.vu018@gmail.com>
2025-03-19 15:46:27 -0400

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,11 @@ function App() {
         </h1>
       </div>
       <div className='flex flex-row w-full justify-center gap-10 my-4'>
-        <LabeledTextInput id="scouter-name" name="scouter-name">Scouter Name</LabeledTextInput>
+        <LabeledTextInput id="scouter-name" name="scouter-name" width='10'>Scouter Name</LabeledTextInput>
       </div>
       <div className='flex flex-row w-full justify-center gap-10 my-4'>
-        <LabeledTextInput id="team-number" name="team-number">Team<br/>Number</LabeledTextInput>
-        <LabeledTextInput id="match-number" name="match-number">Match<br/>Number</LabeledTextInput>
+        <LabeledTextInput id="team-number" name="team-number" width='10'>Team<br/>Number</LabeledTextInput>
+        <LabeledTextInput id="match-number" name="match-number" width='10'>Match<br/>Number</LabeledTextInput>
       </div>
     </>
   )
--- src/components/LabeledTextInput.tsx
+++ src/components/LabeledTextInput.tsx
@@ -1,5 +1,3 @@
-import React, { Children } from 'react';
-
 interface Properties {
     children: any;
     id: string;
@@ -7,10 +5,10 @@ interface Properties {
     width?: string;
 }
 
-const LabeledTextInput = ({id, name, children, width="4"}: Properties) => {
+const LabeledTextInput = ({id, name, children, width="100"}: Properties) => {
   return (
-    <div className='flex flex-col align-center'>
-        <label className={`text-md text-center w-${width}`} htmlFor={id}>{children}</label>
+    <div className={`flex flex-col w-${width} align-center`}>
+        <label className="text-md text-center" htmlFor={id}>{children}</label>
         <input type="text" className='border-b-2 border-white text-md text-center' id={id} name={name}></input>
     </div>
   )
