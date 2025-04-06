b816beaf0d4c65420ab1c8b31b4b982fb73f7443
TVA018 <thomas.vu018@gmail.com>
2025-04-06 17:05:22 -0400

    Display context data if it already exists when switching between routes/pages

--- src/components/Home/Page.tsx
+++ src/components/Home/Page.tsx
@@ -11,12 +11,12 @@ function Page() {
   const context = useContext(AppContext);
 
   // Initialize state hooks
-  [context.scouterName, context.setScouterName] = useState();
-  [context.matchNumber, context.setMatchNumber] = useState();
-  [context.profile, context.setProfile] = useState(0);
-  [context.team, context.setTeam] = useState();
-  [context.teamNumbers, context.setTeamNumbers] = useState();
-  [context.flipField, context.setFlipField] = useState(false);
+  [context.scouterName, context.setScouterName] = useState(context.scouterName);
+  [context.matchNumber, context.setMatchNumber] = useState(context.matchNumber);
+  [context.profile, context.setProfile] = useState(context.profile);
+  [context.team, context.setTeam] = useState(context.team);
+  [context.teamNumbers, context.setTeamNumbers] = useState(context.teamNumbers);
+  [context.flipField, context.setFlipField] = useState(context.flipField);
 
   return (
     <>
@@ -26,8 +26,8 @@ function Page() {
         </h1>
         <form className="flex flex-col justify-center w-full gap-10">
           <div className='flex flex-row w-full justify-center gap-10 my-4'>
-            <LabeledInput inputType='text' id="scouter-name" name="scouter-name" width='w-40' onChange={context.setScouterName} >Scouter Name</LabeledInput>
-            <LabeledInput inputType='number' id="match-number" name="match-number" width='w-40' onChange={context.setMatchNumber} >Match Number</LabeledInput>
+            <LabeledInput inputType='text' id="scouter-name" name="scouter-name" width='w-40' initialText={context.scouterName} onChange={context.setScouterName} >Scouter Name</LabeledInput>
+            <LabeledInput inputType='number' id="match-number" name="match-number" width='w-40' initialText={context.matchNumber?.toString()} onChange={context.setMatchNumber} >Match Number</LabeledInput>
           </div>
           <ProfileSelection />
           <RetrieveDataButton />
@@ -36,7 +36,7 @@ function Page() {
           <img className={`w-150 h-auto${context.flipField ? " scale-x-[-1]" : ""}`} src={fieldImage} alt="image of field" />
         </div>
         <div className="flex flex-row justify-center w-full my-0">
-          <input type="checkbox" id="flip-field" onChange={(e) => context.setFlipField && context.setFlipField(e.target.checked)} />
+          <input type="checkbox" id="flip-field" defaultChecked={context.flipField} onChange={(e) => context.setFlipField && context.setFlipField(e.target.checked)} />
           <label className="text-white" htmlFor="flip-field" >Flip Field</label>
         </div>
       </div>
--- src/gui/LabeledInput.tsx
+++ src/gui/LabeledInput.tsx
@@ -3,6 +3,7 @@ import {Dispatch, ReactNode, SetStateAction} from "react";
 
 interface Properties {
     children: ReactNode;
+    initialText?: string;
     id: string;
     name: string;
     width?: string;
@@ -10,13 +11,13 @@ interface Properties {
     onChange: Dispatch<SetStateAction<any>>; // What value of the app data does this input change
 }
 
-const LabeledInput = ({id, name, children, width, inputType = "text", onChange}: Properties) => {
+const LabeledInput = ({id, name, children, initialText, width, inputType = "text", onChange}: Properties) => {
   const divTailwindCSS = `flex flex-col ${width} align-center`;
 
   return (
     <div className={divTailwindCSS}>
         <label className="text-md text-center text-white w-full" htmlFor={id}>{children}</label>
-        <input type={inputType} className='text-white border-b-2 border-white text-md text-center w-full' id={id} name={name} onChange={(e) => onChange(e.target.value)}></input>
+        <input type={inputType} className='text-white border-b-2 border-white text-md text-center w-full' defaultValue={initialText} id={id} name={name} onChange={(e) => onChange(e.target.value)}></input>
     </div>
   )
 }
