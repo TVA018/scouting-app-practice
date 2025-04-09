55fff61947416a859fe61a3b33e207a494366ea8
TVA018 <thomas.vu018@gmail.com>
2025-04-08 23:04:57 -0400

    Add even more padding

--- src/components/Home/Page.tsx
+++ src/components/Home/Page.tsx
@@ -20,7 +20,7 @@ function Page() {
 
   return (
     <>
-      <div className="flex flex-col w-full justify-start pb-4">
+      <div className="flex flex-col w-full justify-start pb-6">
         <h1 className="text-4xl text-center text-white font-bold my-4">
           Home
         </h1>
